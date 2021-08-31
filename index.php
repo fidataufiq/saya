bf659e86733d9fe839f47575b52d9b1661cdea6b
fidataufiq <fidataufiqimazuddin@gmail.com>
2021-08-31 07:48:40 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<title>Contoh list dengan PHP</title>
+
+</head>
+
+<body>
+
+<h2>Daftar Absensi Mahasiswa</h2>
+
+<ol>
+
+<?php
+
+for($i= 1; $i<= 10; $i++)
+
+{
+
+echo”<li>Nama Mahasiswa ke-$i</li>”;
+
+}
+
+?>
+
+</ol
+
+</body>
+
+</html>
